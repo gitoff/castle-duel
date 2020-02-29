71d0e92d3a1770bb75fed5d3bc8a12505e71568a
One Neo Eon <513@stevebau.com>
2020-02-29 01:31:52 -0500

    initial castle banners. add food and health icon. add foodRatio and healthRatio.

--- components/world.js
+++ components/world.js
@@ -7,7 +7,32 @@ Vue.component('castle', {
   <div class="castle" :class="'player-' + index">
     <img class="building" :src="'svg/castle' + index + '.svg'" />
     <img class="ground" :src="'svg/ground' + index + '.svg'" />
-    <!-- Later, we will add a castle-banners component here -->
+    <castle-banners :player="player" />
   </div>`,
   props: ['player', 'index'],
+})
+
+Vue.component('castle-banners', {
+  template: `
+  <div class="banners">
+    <!-- Food -->
+    <img class="food-icon" src="svg/food-icon.svg" />
+    <!-- Bubble here -->
+    <!-- Banner bar here -->
+    <!-- Health -->
+    <img class="health-icon" src="svg/health-icon.svg" />
+    <!-- Bubble here -->
+    <!-- Banner bar here -->
+  </div>`,
+  props: ['player'],
+
+  computed: {
+    foodRatio () {
+      // maxFood and maxHleath defined in state.js
+      return this.player.food / maxFood
+    },
+    healthRatio () {
+      return this.player.health / maxHealth
+    },
+  }
 })
