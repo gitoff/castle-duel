92376efbe05416e11a204c769a491656bf2b46a0
One Neo Eon <513@stevebau.com>
2020-02-28 20:57:06 -0500

    add state.js and utils.js

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
 new Vue ({
   name: 'game',
   el: '#app',
+
+  data: state,
+
+  template: `<div id="#app">
+    Hello world!
+  </div>`,
+
+  mounted () {
+    console.log(this.$data === state)
+  },
+})
+
+// Window resize handling
+window.addEventListener('resize', () => {
+  state.worldRatio = getWorldRatio()
 })
