041c1c960edd75ca4996228ae2a5ea33a52aa7c0
One Neo Eon <513@stevebau.com>
2020-02-29 01:19:50 -0500

    add transition fade to overlay-background

--- main.js
+++ main.js
@@ -12,6 +12,9 @@ new Vue ({
     <transition name="hand">
       <hand :cards="testHand" v-if="!activeOverlay" @card-play="testPlayCard" />
     </transition>
+    <transition name="fade">
+      <div class="overlay-background" v-if="activeOverlay" />
+    </transition>
     <transition name="zoom">
       <overlay v-if="activeOverlay" :key="activeOverlay">
         <component :is="'overlay-content-' + activeOverlay"
